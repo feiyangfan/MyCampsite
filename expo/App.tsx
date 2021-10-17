7338eaf98f2909c9ab4b349d5adfc420e0449d3f
Chaoyu Liu <kevinlcy.liu@mail.utoronto.ca>
2021-10-17 19:24:15 -0400

    firebase setup

--- expo/App.tsx
+++ expo/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import {initializeApp} from 'firebase/app'
 import HomeScreen from './src/ts/screens/HomeScreen';
 import MapScreen from './src/ts/screens/MapScreen';
+import {firebaseConfig} from "./src/ts/lib/config"
+
+initializeApp(firebaseConfig)
 
 const Stack = createNativeStackNavigator();
 
---
+++ expo/src/ts/lib/config.ts
@@ -0,0 +1,17 @@
+import {releaseChannel} from "expo-updates"
+
+export const env = {
+    releaseChannel,
+    // baseURL:
+}
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyB6zqNLSfM2plie9yskADL9HqxGUzMpvsM",
+    authDomain: "my-campsite-329022.firebaseapp.com",
+}
+
+export const expoAuthConfig = {
+    google: {
+        expoClientId: "1087026256301-bb4r0q3onn91j0s3a8re86bucd2gv2e0.apps.googleusercontent.com"
+    }
+}
