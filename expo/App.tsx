e3c3815cecaf02b23057850060417a1208a5846d
Kirby Chin <kirby.chin@mail.utoronto.ca>
2021-10-17 17:11:32 -0400

    Moved frontend to subfolder

--- expo/App.tsx
+++ expo/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import HomeScreen from './src/js/screens/HomeScreen';
-import MapScreen from './src/js/screens/MapScreen';
+import HomeScreen from './src/ts/screens/HomeScreen';
+import MapScreen from './src/ts/screens/MapScreen';
 
 const Stack = createNativeStackNavigator();
 
