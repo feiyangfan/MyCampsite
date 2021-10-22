568a4ac3cc78cc355bfcecf852bfdc18c2b465a4
Feiyang Fan <feiyang.fan@mail.utoronto.ca>
2021-10-22 09:28:59 -0400

    Revert "Updated express test to close database connection after testing"
    This reverts commit b790f11f33bd6adac05c223463e634b47ae32d0a.

--- express/__tests__/test.js
+++ express/__tests__/test.js
@@ -1,11 +1,5 @@
-import mongoose from "mongoose";
 import request from "supertest";
 import app from "../app.js";
-
-afterAll((done) => {
-  mongoose.connection.close();
-  done();
-});
 
 test("test index page", async () => {
   const res = await request(app).get("/");
