8b78532c3f17043a19827f80900637f7b5e7dbb1
Feiyang Fan <feiyang.fan@mail.utoronto.ca>
2021-10-22 08:32:16 -0400

    add middleware for mongo connection

--- express/app.js
+++ express/app.js
@@ -34,5 +34,25 @@ app.use((req, res, next) => {
 app.use("/", indexRouter);
 app.use("/location", locationRouter);
 
+// middleware for mongo connection error for routes that need it
+const mongoChecker = (req, res, next) => {
+  // check mongoose connection established.
+  if (mongoose.connection.readyState != 1) {
+    log("Issue with mongoose connection");
+    res.status(500).send("Internal server error");
+    return;
+  } else {
+    next();
+  }
+};
+
+// checks for first error returned by promise rejection if Mongo database suddenly disconnects
+const isMongoError = (error) => {
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    error.name === "MongoNetworkError"
+  );
+}
 
 export default app;
