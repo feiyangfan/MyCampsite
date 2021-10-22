0d83caad4b99fe415d2382129c126b68dd254abe
Feiyang Fan <feiyang.fan@mail.utoronto.ca>
2021-10-22 10:05:01 -0400

    move connection back to app.js

--- express/app.js
+++ express/app.js
@@ -6,9 +6,7 @@ import mongoose from "mongoose";
 
 import dotenv from 'dotenv'
 dotenv.config()
-
-import { mongoose } from "./db/mongoose";
-mongoose.set("useFindAndModify", false); // for some deprecation issues
+import mongoose from 'mongoose';
 
 // ES6 code needed for __dirname to work below
 import { fileURLToPath } from "url";
@@ -37,6 +35,15 @@ app.use((req, res, next) => {
 app.use("/", indexRouter);
 app.use("/location", locationRouter);
 
+/* Connect to our database */
+// Get the URI of the local database, or the one specified on deployment.
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/301project'
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+.catch((error) => {
+    console.log('Error connecting to mongodb. Timeout reached.', error);
+  });
+mongoose.set("useFindAndModify", false); // for some deprecation issues
 
 // middleware for mongo connection error for routes that need it
 const mongoChecker = (req, res, next) => {
