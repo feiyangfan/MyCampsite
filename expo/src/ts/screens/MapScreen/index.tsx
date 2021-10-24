bb4ad2b2a51355da671aaf89171f2c833a28c3a5
Kirby Chin <kirby.chin@mail.utoronto.ca>
2021-10-23 22:16:50 -0400

    Sync react-native-maps with expo-three using spherical coordinates

--- expo/src/ts/screens/MapScreen/index.tsx
+++ expo/src/ts/screens/MapScreen/index.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useRef, useEffect} from 'react';
-import {View, StyleSheet, Dimensions} from 'react-native';
+import {View, StyleSheet, Dimensions, AppState} from 'react-native';
 import MapView, { Camera, PROVIDER_GOOGLE }  from 'react-native-maps';
 import {Text, Button} from "react-native-elements";
 import * as Location from 'expo-location';
@@ -59,18 +59,26 @@ const MapScreen = ({ navigation }: Types.MapScreenNavigationProp) => {
           heading: 0,
           zoom: 20
         }, { duration: 700 });
-        // setButtonMsg("world, " + camera.altitude);
-      } else {
-        // setButtonMsg("world2");
       }
     }
   }, [mapLoaded, location])
 
+  const storage: Camera = {
+    pitch: 0,
+    zoom: 0,
+    heading: 0,
+    altitude: 0,
+    center: {
+      latitude: 0,
+      longitude: 0
+    }
+  }
+  const appState = useRef(storage);
+
   const onMapPress = async () => {
-    // setButtonMsg(prev => prev + "p");
     if (map.current) {
       let camera: Camera = await map.current.getCamera();
-      setButtonMsg("camera " + camera.zoom);
+      appState.current = camera;
     }
   }
 
@@ -115,35 +123,45 @@ const MapScreen = ({ navigation }: Types.MapScreenNavigationProp) => {
           onContextCreate={async (gl: ExpoWebGLRenderingContext) => {
             const renderer = new ExpoTHREE.Renderer({ gl });
             renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
-
             const _scene = new THREE.Scene(); 
+            const sceneColor = 0x6ad6f0;
+            _scene.fog = new THREE.Fog(sceneColor, 1, 10000);
             _scene.add(new THREE.GridHelper(10, 10));
-            const _camera = new THREE.PerspectiveCamera(75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000);   
-            _camera.position.y = 10;
-            _camera.position.z = 0;
             
             // load model
             const model = {
-              'robot.glb': require('./models/robot.glb')
+              'robot.glb': require('../../../../assets/models/robot.glb')
             };
 
             const gltf = await ExpoTHREE.loadAsync(model['robot.glb']);
             const object = gltf.scene;
             ExpoTHREE.utils.scaleLongestSideToSize(object, 3);
             _scene.add(object);
+
+            const _camera = new THREE.PerspectiveCamera(75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000);   
+            _camera.position.y = 10;
+            _camera.position.z = 0;
+            _camera.lookAt(object.position);
   
             const light = new THREE.DirectionalLight(0xFFFFFF, 1);
             light.position.y = 3;
             light.position.z = 5;
             _scene.add(light);
 
-            function update() {
-
-            }
-
             const render = () => {
               timeout = requestAnimationFrame(render);
-              update();
+              if (appState.current) {
+                let r = Math.max(20 - appState.current.zoom, 0) * 10;
+                let theta = THREE.MathUtils.degToRad(appState.current.pitch);
+                let phi = THREE.MathUtils.degToRad(appState.current.heading);
+                // basic trig to get camera position
+                _camera.position.set(r * Math.sin(theta) * Math.cos(phi), r * Math.cos(theta), r * Math.sin(theta) * Math.sin(phi));
+              } else {
+                _camera.position.set(0, 10, 0);
+              }
+              _camera.lookAt(object.position);
+              setButtonMsg("camera1 " + appState.current.heading);
+              
               renderer.render(_scene, _camera);
               gl.endFrameEXP();
             };
@@ -171,6 +189,8 @@ const MapScreen = ({ navigation }: Types.MapScreenNavigationProp) => {
         scrollDuringRotateOrZoomEnabled={false}
         loadingIndicatorColor={"#606060"}
         loadingBackgroundColor={"#FFFFFF"}
+        minZoomLevel={15}
+        maxZoomLevel={18}
       >
         {campsiteMarkers.map(obj => <MapCampsiteMarker key={obj.num} num={obj.num} latitude={obj.latitude} longitude={obj.longitude} />)}
       </MapView>
