1add2bda0af7896e2b2078f24bbb9b798c99b3cb
Chaoyu Liu <kevinlcy.liu@mail.utoronto.ca>
2021-11-25 15:11:45 -0500

    d3 and android integration

--- expo/src/ts/lib/config.ts
+++ expo/src/ts/lib/config.ts
@@ -4,6 +4,8 @@ import {ResponseType} from "expo-auth-session"
 let baseURL = "http://10.0.2.2:3000"
 if (releaseChannel.startsWith("prod-d2"))
     baseURL = "https://mycampsite-team12.herokuapp.com"
+else if (releaseChannel.startsWith("prod-d3"))
+    baseURL = "https://mycampsite-team12.herokuapp.com" // TODO change this
 export const env = {
     releaseChannel,
     baseURL
@@ -16,10 +18,12 @@ export const firebaseConfig = {
 
 export const expoAuthConfig = {
     google: {
-        expoClientId: "1087026256301-bb4r0q3onn91j0s3a8re86bucd2gv2e0.apps.googleusercontent.com"
+        expoClientId: "1087026256301-bb4r0q3onn91j0s3a8re86bucd2gv2e0.apps.googleusercontent.com",
+        androidClientId: "1087026256301-pk17qjncsrhncbsbaqm170oskncq8b8d.apps.googleusercontent.com"
     },
     facebook: {
         expoClientId: "178836534456775",
+        androidClientId: "178836534456775",
         responseType: ResponseType.Token
     }
 }
