6e1377a10117713a0b4fe7c8e61094d747da9976
Janine <janine.newton@mail.utoronto.ca>
2021-11-25 22:02:37 -0500

    Update AddSite screen

--- express/models/Park.js
+++ express/models/Park.js
@@ -1,22 +1,10 @@
 import mongoose from "mongoose";
 import { SiteSchema } from "./Site.js";
-// const ParkBoundarySchema = mongoose.Schema({
-//   latitudeStart: { type: Number, required: true },
-//   latitudeEnd: { type: Number, required: true },
-//   longitudestart: { type: Number, required: true },
-//   longitudeEnd: { type: Number, required: true },
-// });
-
-// const ParkLocationSchema = mongoose.Schema({
-//   latitude: { type: Number, required: true },
-//   longitude: { type: Number, required: true },
-//   radius: { type: Number, default: -1 },
-// });
 
 const ParkSchema = mongoose.Schema({
   name: { type: String, required: true },
-  boundary: { type: Object, required: true }, // Can type be BoundarySchema?
-  location: { type: Object, required: true }, // Can type be LocationSchema?
+  boundary: { type: Object, required: true },
+  location: { type: Object, required: true },
   sites: { type: [SiteSchema], default: [] },
 });
 
