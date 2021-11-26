5127ad5355bea66583ddc007cb2b323a14c6697e
Feiyang Fan <feiyang.fan@mail.utoronto.ca>
2021-11-25 22:34:57 -0500

    finished spotlight card

---
+++ expo/src/ts/components/SpotlightCard/index.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+
+const SpotlightCard = (props: any) => {
+  const { siteId, siteName, siteImage } = props;
+  //console.log(siteId, siteName, siteImage);
+  if (siteImage) {
+    return (
+      <View>
+        <TouchableOpacity
+          style={styles.card}
+          onPress={() => props.onSpotlightSelect(siteId, siteName)}
+        >
+          <ImageBackground
+            source={siteImage}
+            resizeMode="cover"
+            style={styles.image}
+            imageStyle={{ borderRadius: 25 }}
+          >
+            <Text style={styles.cardText}>{siteName}</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+      </View>
+    );
+  } else {
+    return (
+      <View>
+        <TouchableOpacity
+          style={[styles.card, { borderRadius: 25, backgroundColor: "white" }]}
+          onPress={() => props.onSpotlightSelect(siteId, siteName)}
+        >
+          <Text style={[styles.cardText, { color: "#005131" }]}>
+            {siteName}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  cardText: {
+    padding: 10,
+    textAlign: "center",
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+    textShadowColor: "rgba(0, 0, 0, 0.75)",
+    textShadowOffset: { width: 5, height: 5 },
+    textShadowRadius: 3,
+  },
+  card: {
+    margin: 6,
+    width: 150,
+    height: 250,
+    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
+
+export default SpotlightCard;
